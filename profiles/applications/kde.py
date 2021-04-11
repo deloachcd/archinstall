2f45dda52a91cf017021495d29a875a09b6000dc
Chandler DeLoach <deloach.cdd@gmail.com>
2021-04-11 00:08:47 -0600

    remove kate, add kde-system-meta

--- profiles/applications/kde.py
+++ profiles/applications/kde.py
@@ -1,5 +1,5 @@
 import archinstall
-packages = "plasma konsole kate dolphin sddm plasma-wayland-session"
+packages = "plasma kde-system-meta konsole dolphin sddm plasma-wayland-session"
 if "nvidia" in _gfx_driver_packages:
 	packages = packages + " egl-wayland"
 installation.add_additional_packages(packages)
